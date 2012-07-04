c551720b6d784a8d9da370e0aefe042bae1c5daa
jhsiao <blaze3j@yahoo.com>
2012-07-04 11:26:13 -0700

    Base for Tests

---
+++ test/distributed/hash/table/TestRequest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package distributed.hash.table;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author jhsiao
+ *
+ */
+public class TestRequest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link distributed.hash.table.Request#Request(int, int, int, java.lang.Object)}.
+     */
+    @Test
+    public void testRequest() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link distributed.hash.table.Request#getKey()}.
+     */
+    @Test
+    public void testGetKey() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link distributed.hash.table.Request#getRequestId()}.
+     */
+    @Test
+    public void testGetRequestId() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link distributed.hash.table.Request#getMachineId()}.
+     */
+    @Test
+    public void testGetMachineId() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link distributed.hash.table.Request#getValue()}.
+     */
+    @Test
+    public void testGetValue() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link distributed.hash.table.Request#toString()}.
+     */
+    @Test
+    public void testToString() {
+        fail("Not yet implemented");
+    }
+
+}
